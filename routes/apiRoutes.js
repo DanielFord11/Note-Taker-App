b8998eb7bc5c68c846919db719fd8dd78ade52b1
Danny Ford <dannyford11@me.com>
2023-07-06 23:18:26 -0500

    fixed stupid routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,29 +1,80 @@
-//create API Routes
 const router = require('express').Router();
+const fs = require('fs');
+const path = require('path');
+
+const dbFilePath = path.join(__dirname, '../db/db.json');
 
 // GET "/api/notes" responds with all notes from the database
 router.get('/notes', (req, res) => {
-  store
-    .getNotes()
-    .then((notes) => {
-      return res.json(notes);
-    })
-    .catch((err) => res.status(500).json(err));
+  try {
+    console.log('this ran');
+    const db = fs.readFileSync(dbFilePath, 'utf8');
+    const notes = JSON.parse(db);
+    return res.status(200).json(notes);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
+// POST "/notes" handles creating a new note
 router.post('/notes', (req, res) => {
-  store
-    .addNote(req.body)
-    .then((note) => res.json(note))
-    .catch((err) => res.status(500).json(err));
-});
+    try {
+      const { title, text } = req.body;
+  
+      // Read the existing notes from the database file
+      const db = fs.readFileSync(dbFilePath, 'utf8');
+      const notes = JSON.parse(db);
+  
+      // Create a new note object
+      const newNote = {
+        title,
+        text,
+      };
+  
+      // Add the new note to the existing notes array
+      notes.push(newNote);
+  
+      // Write the updated notes back to the database file
+      fs.writeFileSync(dbFilePath, JSON.stringify(notes));
+  
+      return res.status(200).json({ message: 'Note created successfully' });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+  
+  
 
-// DELETE "/api/notes" deletes the note with an id equal to req.params.id
+// DELETE "/api/notes/:id" handles deleting a note by ID
 router.delete('/notes/:id', (req, res) => {
-  store
-    .removeNote(req.params.id)
-    .then(() => res.json({ ok: true }))
-    .catch((err) => res.status(500).json(err));
-});
+    try {
+      const { id } = req.params;
+  
+      // Read the existing notes from the database file
+      const db = fs.readFileSync(dbFilePath, 'utf8');
+      let notes = JSON.parse(db);
+  
+      // Find the index of the note with the provided ID
+      const index = notes.findIndex((note) => note.id === id);
+  
+      // If the note is found, remove it from the notes array
+      if (index !== -1) {
+        notes.splice(index, 1);
+  
+        // Write the updated notes back to the database file
+        fs.writeFileSync(dbFilePath, JSON.stringify(notes));
+  
+        return res.status(200).json({ message: 'Note deleted successfully' });
+      }
+  
+      return res.status(404).json({ error: 'Note not found' });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+  
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,12 +6,16 @@ const htmlRoutes = require('./routes/htmlRoutes');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+//app.set('view engine', 'ejs');
+
 // Set up body parsing, static, and route middleware
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
+app.use('/api', apiRoutes);
+app.use(express.static('public'));
 
 // Start the server on the port
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
